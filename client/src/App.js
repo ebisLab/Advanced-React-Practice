728db33116d397933619962b6787a579c91cd9aa
ebisLab <ebaiden@techie.com>
2020-02-14 08:19:49 -0500

    initial commit and converted page to class

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import './App.css';
 
-function App() {
-  
+class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+players: []
+    }
+  }
+
+  render(){
     return (
-    <div className="App">
-      <h1>Happy building <span role="img" aria-label="Smiling emoji">😃</span></h1>
-    </div>
-  );
+      <div className="App">
+        <h1>Happy building <span role="img" aria-label="Smiling emoji">😃</span></h1>
+      </div>
+    );
+  }
+  
+    
 }
 
 export default App;
